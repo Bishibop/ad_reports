e79a80d67567c692927d89004c0a5062fb1f51f8
Your Name <nwmullen@gmail.com>
2016-05-23 17:25:05 -0400

    adds client creation

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,26 @@
 class ClientsController < ApplicationController
   def index
   end
+
+  def show
+    @client = Client.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render status: 404, text: "Client not found."
+  end
+
+  def new
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    @client.save
+    redirect_to @client
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit([:name])
+  end
 end
