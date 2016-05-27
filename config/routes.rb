c7f0b4b563f5649a1203ea99ea73ff4a19750011
Your Name <nwmullen@gmail.com>
2016-05-27 13:23:35 -0400

    fixed type in auth0 failure route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/auth/auth0/callback' => "auth0#callback"
 
-  get '/auth/failure' => "auth#failure"
+  get '/auth/failure' => "auth0#failure"
 
   get '/dashboard' => "dashboards#netsearch_demo"
 
