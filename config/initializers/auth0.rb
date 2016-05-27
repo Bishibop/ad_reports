b1308a9f0298c662a91bfd0f3a4edb3993fb4ab1
Your Name <nwmullen@gmail.com>
2016-05-27 15:06:33 -0400

    sets config vars in each env for authentication

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'kO0tWhIUJshfIB37j9fN15XrOPRIo8De',
-    '5g2Xl9NA2-e9J4mVPM4skqM7waCL7GFOdNDPt624pW7GKPNqoy2ReMv_4eKk1i5P',
+    ENV["AUTH0_CLIENT_ID"],
+    ENV["AUTH0_CLIENT_SECRET"],
     'bishibop.auth0.com',
-    callback_path: "/auth/auth0/callback"
+    callback_path: ENV["AUTH0_CALLBACK_PATH"]
   )
 end
