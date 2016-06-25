102d5c5136128c1ebe70916c889368244007fd01
Your Name <nwmullen@gmail.com>
2016-06-25 02:50:41 -0400

    abstracts out the login domains rule id

--- app/services/update_auth0_login_domains.rb
+++ app/services/update_auth0_login_domains.rb
@@ -18,7 +18,7 @@ class UpdateAuth0LoginDomains
       :token => ENV['AUTH0_API_TOKEN'],
       :domain => ENV['AUTH0_DOMAIN'],
       :api_version => 2
-    }).update_rule("rul_RlxDNkkUTP0NWCWj", {
+    }).update_rule(ENV['AUTH0_LOGIN_DOMAINS_RULE_ID'], {
       "script": "function (user, context, callback) {\n  global.adminEmails = #{admin_emails.to_json};\n  global.customerDomainIdMap = #{customer_domain_id_map.to_json};\n  global.clientDomainIdMap = #{client_domain_id_map.to_json};\n\n  callback(null, user, context);\n}"
     })
   end
