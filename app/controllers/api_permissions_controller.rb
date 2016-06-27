e739c41f215777b9531d033fb5dea01b8fc86f66
Your Name <nwmullen@gmail.com>
2016-06-27 00:43:58 -0400

    reverts adwords api to a single callback url, storing customer_id in the session for admins

--- app/controllers/api_permissions_controller.rb
+++ app/controllers/api_permissions_controller.rb
@@ -19,17 +19,14 @@ class ApiPermissionsController < ApplicationController
 
   def adwords_initiate
 
-    if @current_user.is_admin?
-      callback_url = customer_adwords_callback_url(params[:customer_id])
-    else
-      callback_url = adwords_callback_url
-    end
+    session[:adwords_registration_customer_id] = params[:customer_id] if @current_user.is_admin?
 
     # This is totally the wrong way to get the oauth_url.
     # With Bing, there was just a method you used to explicitly request that
     # url. I should try and find that...Fuck Google's docs. What a joke.
     begin
-      generate_adwords_authenticator.authorize({:oauth2_callback => callback_url})
+      generate_adwords_authenticator.authorize({:oauth2_callback => adwords_callback_url})
+      #redirect_to generate_adwords_authenticator.update!({:oauth2_callback => adwords_callback_url}).authorization_uri.to_s
     rescue AdsCommon::Errors::OAuth2VerificationRequired => e
       redirect_to e.oauth_url.to_s
     end
@@ -38,24 +35,27 @@ class ApiPermissionsController < ApplicationController
   def adwords_callback
 
     if @current_user.is_admin?
-      @customer = Customer.find(params[:customer_id])
+      @customer = Customer.find(session[:adwords_registration_customer_id])
+      session[:adwords_registration_customer_id] = nil
+      redirect_path = customer_api_permissions_path(@customer)
     else
       @customer = @current_user.customer
+      redirect_path = api_permissions_path
     end
 
-    token = generate_adwords_authenticator.authorize({
+    tokens = generate_adwords_authenticator.authorize({
       :oauth2_callback => adwords_callback_url,
       :oauth2_verification_code => params[:code]
     })
 
     @customer.update({
-      adwords_access_token: token[:access_token],
-      adwords_refresh_token: token[:refresh_token],
-      adwords_issued_at: token[:issued_at],
-      adwords_expires_in_seconds: token[:expires_in]
+      adwords_access_token: tokens[:access_token],
+      adwords_refresh_token: tokens[:refresh_token],
+      adwords_issued_at: tokens[:issued_at],
+      adwords_expires_in_seconds: tokens[:expires_in]
     })
 
-    redirect_to api_permissions_path
+    redirect_to redirect_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
   get '/auth/auth0/callback' => "auth0#callback"
   get '/auth/failure' => "auth0#failure"
 
+  get '/api_permissions/adwords/callback' => "api_permissions#adwords_callback", as: :adwords_callback
+
   resources :clients, except: [:new, :create]
   constraints RolesConstraint.new(:customer) do
     resources :clients, only: [:new, :create]
 
     get '/api_permissions' => "api_permissions#index"
     get '/api_permissions/adwords/initiate' => "api_permissions#adwords_initiate", as: :adwords_initiate
-    get '/api_permissions/adwords/callback' => "api_permissions#adwords_callback", as: :adwords_callback
   end
 
   resources :customers do
@@ -21,7 +22,6 @@ Rails.application.routes.draw do
 
       get '/api_permissions' => "api_permissions#index"
       get '/api_permissions/adwords/initiate' => "api_permissions#adwords_initiate", as: :adwords_initiate
-      get '/api_permissions/adwords/callback' => "api_permissions#adwords_callback", as: :adwords_callback
     end
   end
 
